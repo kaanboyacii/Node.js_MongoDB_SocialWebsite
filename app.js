74f0e9beab2caadbdb9294ea4bf76fabdf5c01e0
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-09-19 18:09:39 +0300

    User routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import conn from "./db.js";
 import pageRoute from "./routes/pageRoute.js";
 import photoRoute from "./routes/photoRoute.js";
+import userRoute from "./routes/userRoute.js";
 
 dotenv.config();
 
@@ -17,10 +18,12 @@ app.set('view engine','ejs');
 //static files middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({extended: true }));
 
 //routes
 app.use('/', pageRoute);
 app.use('/photos',photoRoute);
+app.use('/users',userRoute);
 
 app.listen(port,()=>{
     console.log(`App running on port: ${port}`);
---
+++ routes/userRoute.js
@@ -0,0 +1,10 @@
+import express from "express";
+import * as userController from "../controllers/userController.js";
+
+const router = express.Router();
+
+router
+    .route("/register")
+    .post(userController.createUser)
+
+export default router;
