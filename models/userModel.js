a6479f4c3c45100b86292bff7854abd5a4cf48f4
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-09-20 12:48:52 +0300

    Bcrypt install & hash password in user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const { Schema} = mongoose;
 
@@ -23,6 +24,16 @@ const userSchema = new Schema({
     timestamps: true
 }
 );
+
+userSchema.pre('save', function(next) {
+    const user = this;
+    console.log("user pass 1", user.password);
+    bcrypt.hash(user.password, 10, (err, hash) => {
+        user.password = hash;
+        console.log("user pass 1", user.password);
+        next();
+    });
+});
 
 const User = mongoose.model("User",userSchema)
 
