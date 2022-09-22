2b81dbc2562c081ce22ea12d1e95687db28c1abb
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-09-22 15:10:57 +0300

    Logout

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -23,4 +23,11 @@ const getLoginPage = (req, res) => {
     });
 };
 
-export { getIndexPage, getAboutPage, getRegisterPage, getLoginPage };
+const getLogout = (req, res) => {
+    res.cookie('jwt', '', {
+      maxAge: 1,
+    });
+    res.redirect('/');
+  };
+  
+export { getIndexPage, getAboutPage, getRegisterPage, getLoginPage, getLogout };
