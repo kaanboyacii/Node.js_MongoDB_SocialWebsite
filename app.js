3f91f19f7a344a862dc1837c48c901d20339797b
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-09-24 12:00:21 +0300

    install & import cloudinary, fileupload

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ import cookieParser from "cookie-parser";
 import pageRoute from "./routes/pageRoute.js";
 import photoRoute from "./routes/photoRoute.js";
 import userRoute from "./routes/userRoute.js";
-import {checkUser} from "./middlewares/authMiddleware.js"
+import { checkUser } from "./middlewares/authMiddleware.js";
+import fileUpload from "express-fileupload"
+import { v2 as cloudinary } from "cloudinary"
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+});
+
 
 //connect db
 conn();
@@ -15,20 +24,21 @@ conn();
 const app = express();
 const port = process.env.PORT;
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //static files middleware
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(fileUpload({ useTempFiles: true }));
 
 //routes
-app.use("*",checkUser);
+app.use("*", checkUser);
 app.use('/', pageRoute);
-app.use('/photos',photoRoute);
-app.use('/users',userRoute);
+app.use('/photos', photoRoute);
+app.use('/users', userRoute);
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`App running on port: ${port}`);
 });
