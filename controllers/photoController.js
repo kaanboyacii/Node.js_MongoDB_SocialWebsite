353cedc58f3f085b2c6577755b16cadc8d495e76
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-10-04 12:56:02 +0300

    Photo update route & controller

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -10,9 +10,6 @@ const createPhoto = async (req, res) => {
             folder: 'lenslight_tr',
         }
     );
-    console.log('RESULT::', result);
-
-
     try {
         await Photo.create({
             name: req.body.name,
@@ -73,20 +70,56 @@ const getAPhoto = async (req, res) => {
 
 const deletePhoto = async (req, res) => {
     try {
-      const photo = await Photo.findById(req.params.id);
-  
-      const photoId = photo.image_id;
-  
-      await cloudinary.uploader.destroy(photoId);
-      await Photo.findOneAndRemove({ _id: req.params.id });
-  
-      res.status(200).redirect('/users/dashboard');
-    } catch (error) {
-      res.status(500).json({
-        succeded: false,
-        error,
-      });
-    }
-  };
+        const photo = await Photo.findById(req.params.id);
 
-export { createPhoto, getAllPhotos, getAPhoto, deletePhoto };
+        const photoId = photo.image_id;
+
+        await cloudinary.uploader.destroy(photoId);
+        await Photo.findOneAndRemove({ _id: req.params.id });
+
+        res.status(200).redirect('/users/dashboard');
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error,
+        });
+    }
+};
+
+const updatePhoto = async (req, res) => {
+    try {
+        const photo = await Photo.findById(req.params.id);
+
+        if (req.files) {
+            const photoId = photo.image_id;
+            await cloudinary.uploader.destroy(photoId);
+
+            const result = await cloudinary.uploader.upload(
+                req.files.image.tempFilePath,
+                {
+                    use_filename: true,
+                    folder: 'lenslight_tr',
+                }
+            );
+
+            photo.url = result.secure_url;
+            photo.image_id = result.public_id;
+
+            fs.unlinkSync(req.files.image.tempFilePath);
+        }
+
+        photo.name = req.body.name;
+        photo.description = req.body.description;
+
+        photo.save();
+
+        res.status(200).redirect(`/photos/${req.params.id}`);
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error,
+        });
+    }
+};
+
+export { createPhoto, getAllPhotos, getAPhoto, deletePhoto, updatePhoto };
